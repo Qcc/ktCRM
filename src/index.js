82ab31025521af384923f33a03f70efa9ded9b6e
kevin <kevin.e@qq.com>
2017-03-30 14:14:50 +0800

    crm init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import MainPage from './pages/mainPage';
+import './index.css';
+
+ReactDOM.render(
+  <MainPage />,
+  document.getElementById('root')
+);
